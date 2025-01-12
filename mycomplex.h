47522acbe501812bd3073d02de5029f7e5aff5b1
Vlad Schelkanov <vonokleshi4ka@mail.ru>
2025-01-12 21:08:22 +0500

    Расставлены пробелы

--- mycomplex.h
+++ mycomplex.h
@@ -7,33 +7,33 @@ class Complex {
     double Im;
 
 public:
-    Complex(double aRe=0, double aIm=0);
+    Complex(double aRe = 0, double aIm = 0);
     Complex(const Complex&);
     ~Complex();
-    void Set(double aRe, double aIm=0);
+    void Set(double aRe, double aIm = 0);
     operator double();
     double abs();
-    friend istream& operator>>(   istream&,Complex&) ;
-    friend ostream& operator<< (ostream&, Complex & );
-    Complex operator+ (const Complex&);
-    Complex operator- (const Complex&);
-    Complex operator+ (const double&);
-    friend 		Complex operator+ (const double&, const Complex&);
-    Complex operator- (const double&);
-    friend Complex operator- (const 	double&, const Complex&);
-    Complex operator* (const Complex&);
-    Complex operator* (const double&);
-    friend Complex operator* (const double&, const Complex&)    ;
-    Complex operator/ (const double&);
-    Complex& operator+= (const Complex&);
-    Complex  &  operator-= (const Complex&)    ;
-    Complex& operator*= (const Complex& );
-    Complex& operator  += (const double&);
-    Complex& operator-= (const double&);
-    Complex  &operator  *=(const double  & );
-    Complex  & operator /=( const   double & );
-    Complex& operator = (  const   Complex&);
-    Complex & operator= (const double&);
+    friend istream& operator>>(istream&,Complex&);
+    friend ostream& operator<<(ostream&, Complex&);
+    Complex operator+(const Complex&);
+    Complex operator-(const Complex&);
+    Complex operator+(const double&);
+    friend Complex operator+(const double&, const Complex&);
+    Complex operator-(const double&);
+    friend Complex operator-(const 	double&, const Complex&);
+    Complex operator*(const Complex&);
+    Complex operator*(const double&);
+    friend Complex operator*(const double&, const Complex&);
+    Complex operator/(const double&);
+    Complex& operator+=(const Complex&);
+    Complex& operator-=(const Complex&);
+    Complex& operator*=(const Complex&);
+    Complex& operator+=(const double&);
+    Complex& operator-=(const double&);
+    Complex& operator*=(const double&);
+    Complex& operator/=(const double&);
+    Complex& operator=(const Complex&);
+    Complex& operator=(const double&);
 };
 
 #endif
